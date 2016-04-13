cd15387bfc279d48edda049f16d96f50c6bc2788
Marco Metz <metz@ikusei.de>
2016-04-13 12:18:12 +0200

    styling verbessert

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,3 +35,21 @@ $(document).ready(function() {
   $('.wysiwyg').wysihtml5();
 
 });
+
+
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top + 0;
+    if (window_top > div_top) {
+        $('.sticky').addClass('stick');
+        $('#sticky-anchor').height($('.sticky').outerHeight());
+    } else {
+        $('.sticky').removeClass('stick');
+        $('#sticky-anchor').height(0);
+    }
+}
+
+$(function() {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
