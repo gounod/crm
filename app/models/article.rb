b7a54e921f87289e4055737e85d68005232e8c7d
Marco Metz <metz@ikusei.de>
2016-06-29 00:33:17 +0200

    neues layout

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,14 +14,21 @@ class Article < ActiveRecord::Base
   end
 
   def send_notifications
+    send_mail_to_users = []
     #mail an Moderator, wenn er das will
     if self.try(:discussion).try(:user).try(:notify_me_on_new_articles_i_moderate)
-      Mailer.new_article(self.discussion.user,self).deliver
+      unless send_mail_to_users.include?(self.discussion.user)
+        Mailer.new_article(self.discussion.user,self).deliver
+        send_mail_to_users << self.discussion.user
+      end
     end
 
     self.discussion.followers.where(:notify_me_on_new_articles_i_follow => true).each do |user|
       next unless discussion.readable_by(user)
-      Mailer.new_article(user,self).deliver
+      unless send_mail_to_users.include?(user)
+        Mailer.new_article(user,self).deliver
+        send_mail_to_users << user
+      end
     end
   end
 
