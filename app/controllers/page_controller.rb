3a06cd4a66a526f4bcbb918c759db2e397f06e5f
Marco Metz <m.metz@ikusei.de>
2017-01-25 11:38:13 +0100

    Fotoblog Bild deaktiviert

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,10 +2,10 @@
 
 class PageController < ApplicationController
   def start
-    require 'tumblr'
+    #require 'tumblr'
     #user = Tumblr::User.new('marco.metz@gounod.berlin', 'marco4591')
-    Tumblr.blog = 'gounodstr'
-    @last_post = Tumblr::Post.last(:type => :photo)
+    #Tumblr.blog = 'gounodstr'
+    #@last_post = Tumblr::Post.last(:type => :photo)
   end
 
   def contact
